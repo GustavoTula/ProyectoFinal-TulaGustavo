6a819f4d22bd3c1c452cb2bf57ffac97b705ca3a
Gustavo Tula <tula.gustavo.contacto@gmail.com>
2022-10-29 17:21:51 -0300

    Se agregan todos los htmls, urls y views correspondientes

--- ProyectoCoder/AppCoder/urls.py
+++ ProyectoCoder/AppCoder/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import aceites, inicio, nosotros, noticias, vino , lista_vino , espumante , lista_espumante , aceite , lista_aceite, vinos, espumantes
+from .views import aceites, inicio, lista_personal, nosotros, noticias, personal, vino , lista_vino , espumante , lista_espumante , aceite , lista_aceite, vinos, espumantes
 
 urlpatterns = [
     path('agrega-vino/<nombre>/<varietal>/<añada>', vino),
@@ -7,15 +7,16 @@ urlpatterns = [
     path('agrega-espumantes/<nombre>/<varietal>/<añada>', espumante),
     path('lista-espumantes/', lista_espumante),
     path('agrega-aceite/<nombre>/<varietal>', aceite),
-    path('lista-aceite', lista_aceite),
+    path('lista-aceite/', lista_aceite),
+    path('agrega-personal/<nombre>/<apellido>/<cargo>/<email>', personal),
+    path('lista-personal', lista_personal),
 
     path('', inicio, name= "Inicio"),
     path('nosotros/', nosotros, name="Nosotros"),
     path('vinos/', vinos, name="Vinos"),
     path('espumantes/', espumantes, name="Espumantes"),
     path('aceites/', aceites, name="Aceites"),
-    path('equipo/', espumantes, name="Espumantes"),
+    path('equipo/', personal, name="Personal"),
     path('noticias/', noticias, name="Noticias"),
-
 
 ]
